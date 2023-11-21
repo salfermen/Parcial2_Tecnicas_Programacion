823052282191941dae7e1496567903114a6bc28e
Salim Ferez Mendez <123603908+salfermen@users.noreply.github.com>
2023-11-21 14:16:36 -0500

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,18 @@
+/**
+ * Primera clase que es ejecutada
+ */
+public class Principal {
+/**
+ * Primer método que se ejecuta en mi sistema de reserva de vuelos
+ * @param args
+ *
+ * Complejidad temporal: O(1) Tiempo Constante
+ */
+
+public static void main(String[] args) {
+    Vuelo miVuelo = new Vuelo(hora, fecha, cpMax,);
+  
+}
+
+
+}
